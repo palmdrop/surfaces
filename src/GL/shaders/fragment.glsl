f92f49e54ed8d878c7c87a139d47e739b57a49e7
palmdrop <anton@exlex.se>
2021-03-21 15:58:57 +0100

    Use node package for glsl shaders

--- src/GL/shaders/fragment.glsl
+++ src/GL/shaders/fragment.glsl
@@ -2,7 +2,10 @@ precision mediump float;
 
 varying vec3 fragColor;
 
+#pragma glslify: snoise = require(glsl-noise/simplex/2d);
+
 void main()
 {
-    gl_FragColor = vec4(fragColor, 1.0);
+    float n = 1.0 + snoise(gl_FragCoord.xy * 0.01);
+    gl_FragColor = vec4(fragColor * n, 1.0);
 }
