af2d00619d9d71edd13b7ed33e0702e274c62009
palmdrop <anton@exlex.se>
2021-04-13 20:09:46 +0200

    Structure attributes object better

--- src/context/ControllerAttributes.js
+++ src/context/ControllerAttributes.js
@@ -1,3 +1,68 @@
+const noiseSettings = () => {
+    return {
+        value: {
+            frequency: {
+                value: Math.random() * 0.01,
+                min: 0.0000001,
+                max: 0.035,
+                type: "1f",
+            },
+            octaves: {
+                value: 3,
+                type: "1i",
+                min: 1,
+                max: 5,
+                step: 2,
+                marks: [1, 3, 5]
+            },
+            lacunarity: {
+                value: 2.0,
+                type: "1f",
+                min: 0.1,
+                max: 5.0,
+            },
+            persistence: {
+                value: 0.5,
+                type: "1f",
+                min: 0.1,
+                max: 2.0,
+            },
+            amplitude: {
+                value: 1.0,
+                type: "1f",
+                min: 0.1,
+                max: 5.0,
+            },
+            modifications: {
+                value: {
+                    ridgeThreshold: {
+                        value: 1.0,
+                        type: "1f",
+                        min: 0.5,
+                        max: 1.0,
+                    },
+                    pow: {
+                        value: 1.0,
+                        type: "1f",
+                        min: 0.0,
+                        max: 5
+                    }
+                },
+            }
+        },
+        isUniform: true,
+    }
+};
+
+const timeSettings = (value) => {
+    return {
+        value: value,
+        min: -1.0,
+        max: 3,
+    }
+};
+
+
 const attributes = {
     scale: {
         value: 1.0,
@@ -33,170 +98,17 @@ const attributes = {
     },
     animationSpeed: {
         value: {
-            general: {
-                value: 0.2,
-                min: 0.0,
-                max: 3,
-            },
-            source: {
-                value: 1.0,
-                min: 0.0,
-                max: 5,
-            },
-            angleControl: {
-                value: 1.0,
-                min: 0.0,
-                max: 5,
-            },
-            amountControl: {
-                value: 1.0,
-                min: 0.0,
-                max: 5,
-            },
+            general: timeSettings(0.2),
+            source: timeSettings(1.0),
+            angleControl: timeSettings(1.0),
+            amountControl: timeSettings(1.0)
         },
         isUniform: false,
     },
-    source: {
-        value: {
-            frequency: {
-                value: 0.01,
-                type: "1f",
-                min: 0.0000001,
-                max: 0.035
-            },
-            octaves: {
-                value: 3,
-                type: "1i",
-                min: 1,
-                max: 5,
-                step: 2,
-                marks: [1, 3, 5]
-            },
-            lacunarity: {
-                value: 2.0,
-                type: "1f",
-                min: 0.1,
-                max: 5.0,
-            },
-            persistence: {
-                value: 0.5,
-                type: "1f",
-                min: 0.1,
-                max: 2.0,
-            },
-            modifications: {
-                value: {
-                    ridgeThreshold: {
-                        value: 1.0,
-                        type: "1f",
-                        min: 0.5,
-                        max: 1.0,
-                    },
-                    pow: {
-                        value: 1.0,
-                        type: "1f",
-                        min: 0.0,
-                        max: 5
-                    }
-                },
-            }
-        },
-        isUniform: true,
-    },
-    angleControl: {
-        value: {
-            frequency: {
-                value: Math.random() * 0.01,
-                min: 0.0000001,
-                max: 0.035,
-                type: "1f",
-            },
-            octaves: {
-                value: 3,
-                type: "1i",
-                min: 1,
-                max: 5,
-                step: 2,
-                marks: [1, 3, 5]
-            },
-            lacunarity: {
-                value: 2.0,
-                type: "1f",
-                min: 0.1,
-                max: 5.0,
-            },
-            persistence: {
-                value: 0.5,
-                type: "1f",
-                min: 0.1,
-                max: 2.0,
-            },
-            modifications: {
-                value: {
-                    ridgeThreshold: {
-                        value: 1.0,
-                        type: "1f",
-                        min: 0.5,
-                        max: 1.0,
-                    },
-                    pow: {
-                        value: 1.0,
-                        type: "1f",
-                        min: 0.0,
-                        max: 5
-                    }
-                },
-            }
-        },
-        isUniform: true,
-    },
-    amountControl: {
-        value: {
-            frequency: {
-                value: Math.random() * 0.01,
-                min: 0.0000001,
-                max: 0.035,
-                type: "1f"
-            },
-            octaves: {
-                value: 3,
-                type: "1i",
-                min: 1,
-                max: 5,
-                step: 2,
-                marks: [1, 3, 5]
-            },
-            lacunarity: {
-                value: 2.0,
-                type: "1f",
-                min: 0.1,
-                max: 5.0,
-            },
-            persistence: {
-                value: 0.5,
-                type: "1f",
-                min: 0.1,
-                max: 2.0,
-            },
-            modifications: {
-                value: {
-                    ridgeThreshold: {
-                        value: 1.0,
-                        type: "1f",
-                        min: 0.5,
-                        max: 1.0,
-                    },
-                    pow: {
-                        value: 1.0,
-                        type: "1f",
-                        min: 0.0,
-                        max: 5
-                    },
-                },
-            }
-        },
-        isUniform: true,
-    },
+
+    source: noiseSettings(),
+    angleControl: noiseSettings(),
+    amountControl: noiseSettings()
 };
 
 export { attributes }
