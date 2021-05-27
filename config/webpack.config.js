63917d8f009ec97266f67169da64bde5f6d66260
palmdrop <anton@exlex.se>
2021-05-27 10:02:12 +0200

    Document changes made to webpack config

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -535,7 +535,7 @@ module.exports = function (webpackEnv) {
               ),
             },
 
-            // GLSL loader
+            /* CUSTOM-BEGIN: GLSL loader for easy shader source import */
             {
               test: /\.(glsl|frag|vert)$/,
               use: [
@@ -543,6 +543,7 @@ module.exports = function (webpackEnv) {
                 require.resolve('glslify-loader'),
               ]
             },
+            /* CUSTOM-END: GLSL loader */
             // "file" loader makes sure those assets get served by WebpackDevServer.
             // When you `import` an asset, you get its (virtual) filename.
             // In production, they would get copied to the `build` folder.
